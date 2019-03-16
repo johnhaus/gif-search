919f8ae70ed4f6a7405da1bc40a043c8048543cf
johnhaus <john.haus@outlook.com>
2019-03-16 12:04:46 -0300

    added main gif to left side

---
+++ src/components/gif.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Gif extends Component {
+  render() {
+    return (
+      const src = `https://media2.giphy.com/media/${this.props.id}/200.gif`;
+      <img src="" alt="" className="gif"/>
+    );
+  }
+}
+
+export default Gif;
